75f4d0e4f52b544b113a1cdd56f31d749faf160d
Atul Kumar <tyagiatul23799@gmail.com>
2024-06-25 00:45:19 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Dashboard from "./components/Dashboard";
+import { CryptoProvider } from "./context/CryptoContext";
+
+//App is the parent component of dashboard
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <CryptoProvider>
+        <Dashboard />
+      </CryptoProvider>
+    </Provider>
   );
 }
 
